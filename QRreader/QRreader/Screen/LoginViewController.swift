6d11a63efb81d9f4cb6c72977c2d55435235200a
maiko-m <mn.6182.nm@gmail.com>
2020-01-05 03:54:20 +0900

    complete qr reader func v1

--- QRreader/QRreader/Screen/LoginViewController.swift
+++ QRreader/QRreader/Screen/LoginViewController.swift
@@ -9,11 +9,57 @@
 import UIKit
 import AVFoundation
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    private let session = AVCaptureSession()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let discoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera],
+                                                                mediaType: .video,
+                                                                position: .back)
+        
+        let devices = discoverySession.devices
+        if let backCamera = devices.first {
+            do {
+                let deviceInput = try AVCaptureDeviceInput(device: backCamera)
+                if session.canAddInput(deviceInput) {
+                    session.addInput(deviceInput)
+                    
+                    let metadataOutput = AVCaptureMetadataOutput()
+                    
+                    if session.canAddOutput(metadataOutput) {
+                        session.addOutput(metadataOutput)
+                        
+                        metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+                        metadataOutput.metadataObjectTypes = [.qr]
+                        
+                        let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+                        previewLayer.frame = view.bounds
+                        previewLayer.videoGravity = .resizeAspectFill
+                        view.layer.addSublayer(previewLayer)
+                        
+                        session.startRunning()
+                    }
+                }
+            } catch {
+                print("Error occured while creating video device input: \(error)")
+            }
+        }
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        
+        for metadata in metadataObjects as! [AVMetadataMachineReadableCodeObject] {
+            // QRのtype： metadata.type
+            // QRの中身： metadata.stringValue
+            guard let value = metadata.stringValue else { return }
+            print(value)
+            session.stopRunning()
+
+            view.layer.sublayers?.removeAll()
+        }
     }
 }
 
