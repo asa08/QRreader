7dc39ac924bd29a49d03d7a70e523fea2760693f
maiko-m <mn.6182.nm@gmail.com>
2020-01-05 02:42:15 +0900

    fix bundle name

--- QRreader/QRreader/Screen/LoginViewController.swift
+++ QRreader/QRreader/Screen/LoginViewController.swift
@@ -10,11 +10,6 @@ import UIKit
 import AVFoundation
 
 class LoginViewController: UIViewController {
-    
-    let captureSession = AVCaptureSession()
-    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
-    var qrCodeFrameView: UIView?
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,34 +17,4 @@ class LoginViewController: UIViewController {
     }
 }
 
-extension LoginViewController: AVCaptureMetadataOutputObjectsDelegate {
-
-    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
-        self.captureSession.stopRunning()
-        guard let objects = metadataObjects as? [AVMetadataObject] else { return }
-        var detectionString: String? = nil
-        let barcodeTypes = [AVMetadataObject.ObjectType.ean8, AVMetadataObject.ObjectType.ean13]
-        for metadataObject in objects {
-            loop: for type in barcodeTypes {
-                guard metadataObject.type == type else { continue }
-                guard self.capturePreviewLayer.transformedMetadataObjectForMetadataObject(metadataObject) is AVMetadataMachineReadableCodeObject else { continue }
-                if let object = metadataObject as? AVMetadataMachineReadableCodeObject {
-                    detectionString = object.stringValue
-                    break loop
-                }
-            }
-            var text = ""
-            guard let value = detectionString else { continue }
-            text += "読み込んだ値:\t\(value)"
-            text += "\n"
-//            guard let isbn = convartISBN(value) else { continue }
-//            text += "ISBN:\t\(isbn)"
-//            resultTextLabel?.text = text
-            let URLString = String(format: "http://amazon.co.jp/dp/%@", isbn)
-            guard let URL = NSURL(string: URLString) else { continue }
-            UIApplication.sharedApplication().openURL(URL)
-        }
-        self.captureSession.startRunning()
-    }
-}
 
